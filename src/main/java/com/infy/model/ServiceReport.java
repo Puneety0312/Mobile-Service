17ee05daa64e3141511618104cb710c7acc49d7b
Puneet Yadav <puneety0312@gmail.com>
2023-07-02 12:49:56 +0530

    Validation and Model Class done

--- src/main/java/com/infy/model/ServiceReport.java
+++ src/main/java/com/infy/model/ServiceReport.java
@@ -1,7 +1,51 @@
 package com.infy.model;
 
-
+import java.util.List;
 
 public class ServiceReport {
+	private int serviceId;
+	private String brand;
+	private List<String> issues;
+	private float serviceFee;
+	
+	public ServiceReport(int serviceId,String brand,List<String> issues,float serviceFee) {
+		// TODO Auto-generated constructor stub
+		this.brand=brand;
+		this.issues=issues;
+		this.serviceFee=serviceFee;
+		this.serviceId=serviceId;
+	}
+
+	public int getServiceId() {
+		return this.serviceId;
+	}
+
+	public void setServiceId(int serviceId) {
+		this.serviceId = serviceId;
+	}
+
+	public String getBrand() {
+		return this.brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public List<String> getIssues() {
+		return this.issues;
+	}
+
+	public void setIssues(List<String> issues) {
+		this.issues = issues;
+	}
+
+	public float getServiceFee() {
+		return this.serviceFee;
+	}
+
+	public void setServiceFee(float serviceFee) {
+		this.serviceFee = serviceFee;
+	}
 	
 }
--- src/main/java/com/infy/validator/Validator.java
+++ src/main/java/com/infy/validator/Validator.java
@@ -7,36 +7,59 @@ import com.infy.exception.MobileServiceException;
 import com.infy.model.ServiceRequest;
 
 public class Validator {
-
+	ServiceRequest service ;
 	public void validate(ServiceRequest service) throws MobileServiceException{	
 		//your code goes here
+		this.service=service;
+		if(!isValidBrand(service.getBrand())) {
+			throw new MobileServiceException("Validator.INVALID_BRAND");
+		}
+		if(!isValidIssues(service.getIssues())) throw new MobileServiceException(" Validator.INVALID_NO_OF_ISSUES");
+		if(!isValidContactNumber(service.getContactNumber())) throw new MobileServiceException("Validator.INVALID_CONTACT_NUMBER");
+		
+		if(!isValidIMEINumber(service.getiMEINumber())) throw new MobileServiceException("Validator.INVALID_IMEI_NUMBER");
+		if(!isValidCustomerName(service.getCustomerName())) throw new MobileServiceException("Validator.INVALID_CUSTOMER_NAME");
+		
 	}	
-
+	
 	
 	// validates the brand
 	// brand should always start with a upper case alphabet 
 	// and can be followed by one or more alphabets (lower case or upper case) 
 	public Boolean isValidBrand(String brand){
-		return null;
+		if(Character.isUpperCase(brand.charAt(0))) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	
 	// validates the list of issues
 	// checks if the list is null or empty
 	public Boolean isValidIssues(List<String> issues) {
-		return null;
+		return !(issues==null || issues.isEmpty());
 	}
 
 	// validates the IMEINumber
 	// it should be a 16 digit number 
 	public Boolean isValidIMEINumber(Long iMEINumber) {
-		return null;
+		String s= iMEINumber.toString();
+		return s.length()==16;
 	}
 	
 	// validates the contact number
 	// should contain 10 numeric characters and should not contain 10 repetitive characters
 	public Boolean isValidContactNumber(Long contactNumber) {
-		return null;
+		String c = contactNumber.toString();
+		if(c.matches("(\\D?[0-9]{3}\\D?)[\\s][0-9]{3}-[0-9]{4}") && c.matches("\\d{10}"))
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	
@@ -44,7 +67,12 @@ public class Validator {
 	// should contain at least one word and each word separated by a single space should contain at least one letter.
 	// the first letter of every word should be an upper case character 
 	public Boolean isValidCustomerName(String customerName) {
+		if(customerName.matches("\\[A-Za-z]+") && Character.isUpperCase(customerName.charAt(0))) {
+			return true;
+		}
+		else {
+			return false;
+		}
 		
-		return null;
 	}
 }
